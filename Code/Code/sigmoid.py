18089304954c1fced70d34bb1352e474c9243405
Vincent Assoun <VincentAssoun@MBPdeVincent2.eleves.polytechnique.fr polytechnique.fr>
2017-10-17 18:17:24 +0200

    sigmoid

--- Code/Code/sigmoid.py
+++ Code/Code/sigmoid.py
@@ -3,12 +3,18 @@ import numpy as np
 def sigmoid(z):
 
     # SIGMOID returns sigmoid function evaluated at z
-    g = np.zeros(np.shape(z))
-
-    for a in range(len(g)):
-        g[a] = 1/(1+np.exp(-z[a]))
+    size = np.shape(z)
+    g = np.zeros(size)
 
     # ============================= TODO ================================
     # Instructions: Compute sigmoid function evaluated at each value of z.
+
+    if size[1]:
+        for a in range(size[0]):
+            for b in range(size[1]):
+                g[a,b] = 1/(1+np.exp(-z[a,b]))
+    else:
+        for a in range(len(g)):
+        g[a] = 1/(1+np.exp(-z[a]))
 
     return g
